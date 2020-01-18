5a0f2c8bb332f67c857832e1229c6e9be85cf3aa
dealer426 <burnssamuel01@gmail.com>
2020-01-17 21:02:38 -0500

    Initial SuperHero Push

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace sprint2superhero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Superhero superMan = new Superhero("Superman", 100, true, 100, 100);
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Superhero.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sprint2superhero
+{
+    public class Superhero
+    {
+        //Create 4 Fields
+        private string name;
+        private int strengthLevel;
+        private bool hasCape;
+        private int speedLevel;
+        private int healthLevel;
+
+        //Create 4 Properties
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+        public int StrengthLevel
+        {
+            get { return this.strengthLevel; }
+            set { this.strengthLevel = value; }
+        }
+        public bool HasCape
+        {
+            get { return this.hasCape; }
+            set { this.hasCape = value; }
+        }
+        public int SpeedLevel
+        {
+            get { return this.speedLevel; }
+            set { this.speedLevel = value; }
+        }
+        public int HealthLevel
+        {
+            get { return this.healthLevel; }
+            set { this.healthLevel = value; }
+        }
+
+        //Create 3 Constructors
+
+        //Default Contstructor
+        public Superhero()
+        {
+
+        }
+
+        //Constructor 2
+        public Superhero(int strengthLevel, bool hasCape)
+        {
+            StrengthLevel = strengthLevel;
+            HasCape = hasCape;
+        }
+
+        //Constructor 3
+        public Superhero(string name, int strengthLevel, bool hasCape, int speedLevel, int healthLevel)
+        {
+            Name = name;
+            StrengthLevel = strengthLevel;
+            HasCape = hasCape;
+            SpeedLevel = speedLevel;
+            HealthLevel = healthLevel;
+        }
+
+        //Create 2 Methods
+
+        //Impact Super Heros Health
+
+        //Give Super Hero a Power Boost
+    }
+}
